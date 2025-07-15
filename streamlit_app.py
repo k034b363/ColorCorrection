c24d49ed80e43b72138655e7902ec6950767865d
KeelyBrown <156026125+k034b363@users.noreply.github.com>
2025-07-15 11:12:22 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import plantcv.plantcv as pcv
 import cv2
+import numpy as np
 
 # Don't know if this is necessary, but just in case
 pcv.params.debug = None
@@ -10,16 +11,16 @@ st.title("Image Color Correction with PlantCV")
 # File uploader
 uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
+# Add tabs
+tab1, tab2 = st.tabs(["Original", "Color Corrected"])
+
 # Read in array once a file has been uploaded
 if uploaded_file:
     file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
     image = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+    
+    # Do the color correction and show both in tabs
+    cc = pcv.transform.auto_correct_color(image)
 
-# Add tabs
-tab1, tab2 = st.tabs(["Original", "Color Corrected"])
-
-# Do the color correction and show both in tabs
-cc = pcv.transform.auto_correct_color(image)
-
-tab1.image(image, use_column_width=True)
-tab2.image(cc, use_column_width=True)
+    tab1.image(image, use_column_width=True)
+    tab2.image(cc, use_column_width=True)
